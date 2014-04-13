4dde0425480c8b8d1a7662a8557a286d3618670a
rboberg <ross.boberg@gmail.com>
2014-04-13 12:07:34 -0400

    add inline bar chart to ff postmortem position table

--- projects/ff_2013_postmortem/webfiles/main.js
+++ projects/ff_2013_postmortem/webfiles/main.js
@@ -1,10 +1,11 @@
 
+
 //Table of value added by position with embedded bar charts.
 d3.csv('webfiles/value_by_position.csv', function(data) {
 	var COLUMNS = [
-	{colname:'Position',coltext:'Position'},
-	{colname:'ValueAddedPerGame1',coltext:'Value Added (Method 1)'},
-	{colname:'ValueAddedPerGame2',coltext:'Value Added (Method 2)'}
+	{colname:'Position',coltext:'Position',coltype:'tdtext'},
+	{colname:'ValueAddedPerGame1',coltext:'Value Added (Method 1)',coltype:'tdvalue'},
+	{colname:'ValueAddedPerGame2',coltext:'Value Added (Method 2)',coltype:'tdvalue'}
 	];
 
 	var table, thead, tbody;
@@ -17,23 +18,68 @@ d3.csv('webfiles/value_by_position.csv', function(data) {
     .data(COLUMNS)
     .enter()
     .append('th')
-    .text(function(column){return column.coltext});
+    .text(function(column){return column.coltext})
+    .attr('class',function(column){return column.coltype});
 
     var rows = tbody.selectAll('tr')
     .data(data)
     .enter()
     .append('tr');
 
+    
+    //old way of adding tds
     var cells = rows.selectAll('td')
     .data(function(row){
-    	return _.map(COLUMNS,function(column){
-    		return {column:column.colname,cvalue:row[column.colname]};
-    	});
+        return _.map(COLUMNS,function(column){
+            return {column:column.colname,cvalue:row[column.colname],ctype:column.coltype};
+        });
     })
     .enter()
     .append('td')
+    .attr('class',function(d){
+        return d.ctype;
+    });
+
+    rows.selectAll('.tdtext')
     .text(function(d){return d.cvalue})
 
+    rows.selectAll('.tdvalue')
+    .style({'text-align' : 'center'})
+    //.text(function(d){return Math.round(d.cvalue*100)/100})
 
+    
 
+    //Add rects
+    var maxabs = 8  ;
+    var barheight = 20;
+
+    var x1 = d3.scale.linear()
+    .domain([0, maxabs])
+    .range([0, '100%']);
+
+    var svg = rows.selectAll('.tdvalue')
+    .append('svg')
+    .attr('width','100%')
+    .attr('height',barheight);
+
+    var g = svg
+    .append('g')
+    .classed('chart',true);
+
+    var bar = g
+    .append('rect')
+    .attr('width',function(d){return x1(Math.abs(d.cvalue));})
+    .attr('height',barheight)
+    .attr('class',function(d){return d.cvalue < 0?'negbar':'posbar'});
+
+    var text = g
+    .append('text')
+    //.attr('x',function(d){return x1(Math.abs(d.cvalue));})
+    .attr('x','100%')
+    .attr('y',barheight/2)
+    .attr('dy','0.3em')
+    .attr('transform','translate(0)')
+    .text(function(d){return Math.round(d.cvalue*100)/100});
+
+    var i =1;
 })
