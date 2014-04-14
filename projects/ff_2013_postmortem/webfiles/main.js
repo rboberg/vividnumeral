bcb3a1500ce1755e5165cacf4d7e89988d982837
Ross Boberg <ross.boberg@moorecap.com>
2014-04-14 15:11:21 -0400

    begin mock draft visualization

--- projects/ff_2013_postmortem/webfiles/main.js
+++ projects/ff_2013_postmortem/webfiles/main.js
@@ -81,7 +81,6 @@ d3.csv('webfiles/value_by_position.csv', function(data) {
     .attr("x",function(d){
         return d.cvalue < 0?(barcenter - parseFloat(x1(Math.abs(d.cvalue)))) + "%":barcenter + "%"
     })
-    .attr('transform','translate(100%)')
     .attr('class',function(d){return d.cvalue < 0?'negbar':'posbar'});
 
     var text = g
@@ -95,3 +94,71 @@ d3.csv('webfiles/value_by_position.csv', function(data) {
 
     var i =1;
 })
+
+
+
+//Interactive Visualization of Mock Draft
+d3.csv('webfiles/mock_draft.csv',function(data){
+    var margin = {top: 10, right: 10, bottom: 100, left: 40},
+    margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    height2 = 500 - margin2.top - margin2.bottom;
+
+
+    var x = d3.scale.linear().range([0, width]),
+    x2 = d3.scale.linear().range([0, width]),
+    y = d3.scale.linear().range([height, 0]),
+    y2 = d3.scale.linear().range([height2, 0]);
+
+
+    var brush = d3.svg.brush()
+        .x(x2)
+        .on("brush", brushed);
+
+    var area = d3.svg.area()
+        .interpolate("monotone")
+        .x(function(d) { return x(d.date); })
+        .y0(height)
+        .y1(function(d) { return y(d.price); });
+
+    var area2 = d3.svg.area()
+        .interpolate("monotone")
+        .x(function(d) { return x2(d.date); })
+        .y0(height2)
+        .y1(function(d) { return y2(d.price); });
+
+    var svg = d3.select("#draft_viz").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
+      .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var context = svg.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+
+    function brushed() {
+      x.domain(brush.empty() ? x2.domain() : brush.extent());
+      focus.select(".area").attr("d", area);
+      focus.select(".x.axis").call(xAxis);
+    }
+   function type(d) {
+      d.date = parseDate(d.date);
+      d.price = +d.price;
+      return d;
+    }
+
+
+    var stop = 1;
+})
+
