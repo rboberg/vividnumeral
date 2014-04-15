e00770bbf19d8c2de99ad06449212392c4f8815c
Ross Boberg <ross.boberg@moorecap.com>
2014-04-15 09:59:28 -0400

    postmortem: add colors to draft viz, begin mouseover labels, more flexible margining

--- projects/ff_2013_postmortem/webfiles/main.js
+++ projects/ff_2013_postmortem/webfiles/main.js
@@ -102,20 +102,42 @@ var colnumeric = ['DraftPosition','OverallRank','PointsOverRep','PositionRank'];
 var xcol = 'DraftPosition';
 var hcol = 'PointsOverRep';
 
+/*
+var allheight = 500,
+margin[1] = {top: 10, right: 10, bottom: 100, left: 10},
+height[1] = allheight - margin[1].top - margin[1].bottom,
+margin[2] = {top: height[1] + 10, right: 10, bottom: 10, left: 10},
+height[2] = allheight - margin[2].top - margin[2].bottom,
+width = 600 - margin[1].left - margin[1].right;
+*/
+
+var margins = {top: 10, right: 10, bottom: 10, left: 10, inner: 10},
+height = [20,400,80],
+heightAll = height.reduce(function(a,b){return a+b+margins.inner;}) + margins.top + margins.bottom,
+width = 600 - margins.left - margins.right;
+
+var margin = new Array(), istart=0,iend=0,itop,ibottom;
+for(var i=0;i<height.length;i++){
+    istart=iend;
+    itop=(i==0?margins.top:margins.inner)
+    ibottom=(i==(height.length-1)?margins.bottom:0)
+    iend=istart+itop+height[i]+ibottom;
+    margin[i] = {
+        top: istart+itop,
+        right: margins.right,
+        bottom: heightAll - iend + ibottom,
+        left: margins.left
+    }
+};
+
+//debugger;
 
 
-var margin = {top: 10, right: 10, bottom: 100, left: 10},
-margin2 = {top: 430, right: 10, bottom: 10, left: 10},
-width = 600 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom,
-height2 = 500 - margin2.top - margin2.bottom;
 
-
-
-var x = d3.scale.linear().range([0, width]),
+var x1 = d3.scale.linear().range([0, width]),
 x2 = d3.scale.linear().range([0, width]),
-y = d3.scale.linear().range([height,0]), 
-y2 = d3.scale.linear().range([height2,0]); 
+y1 = d3.scale.linear().range([height[1],0]), 
+y2 = d3.scale.linear().range([height[2],0]); 
 
 
 
@@ -125,22 +147,26 @@ var brush = d3.svg.brush()
 
 
 var svg = d3.select("#draft_viz").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("width", width + margin[1].left + margin[1].right)
+    .attr("height", height[1] + margin[1].top + margin[1].bottom);
 
 svg.append("defs").append("clipPath")
     .attr("id", "clip")
   .append("rect")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height[1]);
+
+var label = svg.append("g")
+    .attr("class", "label")
+    .attr("transform", "translate(" + margin[0].left + "," + margin[0].top + ")");
 
 var focus = svg.append("g")
     .attr("class", "focus")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin[1].left + "," + margin[1].top + ")");
 
 var context = svg.append("g")
     .attr("class", "context")
-    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+    .attr("transform", "translate(" + margin[2].left + "," + margin[2].top + ")");
 
 
 d3.csv('webfiles/mock_draft.csv', function(data){
@@ -154,15 +180,15 @@ d3.csv('webfiles/mock_draft.csv', function(data){
     var minh = _.min(_.pluck(data,hcol));
     var amaxh = Math.max(maxh,Math.abs(minh));
 
-    var h = d3.scale.linear().range([0,height*amaxh/(maxh-minh)]),
-    h2 = d3.scale.linear().range([0,height2*amaxh/(maxh-minh)]);
+    var h1 = d3.scale.linear().range([0,height[1]*amaxh/(maxh-minh)]),
+    h2 = d3.scale.linear().range([0,height[2]*amaxh/(maxh-minh)]);
 
-    x.domain(d3.extent(data.map(function(d) { return d[xcol]; })));
-    y.domain(d3.extent(data.map(function(d) { return d[hcol]; })));
-    h.domain([0,amaxh]);
-    x2.domain(x.domain());
-    y2.domain(y.domain());
-    h2.domain(h.domain());
+    x1.domain(d3.extent(data.map(function(d) { return d[xcol]; })));
+    y1.domain(d3.extent(data.map(function(d) { return d[hcol]; })));
+    h1.domain([0,amaxh]);
+    x2.domain(x1.domain());
+    y2.domain(y1.domain());
+    h2.domain(h1.domain());
 
     var bar1 = focus.selectAll('rect')
         .data(data)
@@ -170,11 +196,10 @@ d3.csv('webfiles/mock_draft.csv', function(data){
         .append('rect')
             .attr('class','vizbar')
             .attr('width',barw)
-            .attr('x',function(d){return x(d[xcol]);})
-            .attr('y',function(d){return d[hcol] < 0 ? y(0) : y(d[hcol]);})
-            .attr('height',function(d){return h(Math.abs(d[hcol]));});
-            //.attr('class',function(d){return d[hcol] < 0 ? 'negbar' : 'posbar';});
-
+            .attr('x',function(d){return x1(d[xcol]);})
+            .attr('y',function(d){return d[hcol] < 0 ? y1(0) : y1(d[hcol]);})
+            .attr('height',function(d){return h1(Math.abs(d[hcol]));})
+            .classed('valuebar',function(d){return d['DraftType'] == 'value'});
 
 
     var bar2 = context.selectAll('rect')
@@ -186,14 +211,18 @@ d3.csv('webfiles/mock_draft.csv', function(data){
             .attr('x',function(d){return x2(d[xcol]);})
             .attr('y',function(d){return d[hcol] < 0 ? y2(0) : y2(d[hcol]);})
             .attr('height',function(d){return h2(Math.abs(d[hcol]));})
-            .attr('on');
+            .classed('valuebar',function(d){return d['DraftType'] == 'value'});
+
+    var labelText = label.append('text')
+       .attr({'x':0,'y':0})
+       .text('text');
 
     context.append("g")
       .attr("class", "x brush")
       .call(brush)
     .selectAll("rect")
       .attr("y", -6)
-      .attr("height", height2 + 7);
+      .attr("height", height[2] + 7);
 
 //    debugger;
 
@@ -202,10 +231,10 @@ d3.csv('webfiles/mock_draft.csv', function(data){
 
 // HELPER FUNCTIONS
 function brushed() {
-    x.domain(brush.empty() ? x2.domain() : brush.extent());
+    x1.domain(brush.empty() ? x2.domain() : brush.extent());
     focus.selectAll(".vizbar")
-    .attr('x',function(d){return x(d[xcol]);})
-    .attr('width', Math.floor(width/Math.max(x.domain()[1]-x.domain()[0],1)- 1));
+    .attr('x',function(d){return x1(d[xcol]);})
+    .attr('width', Math.floor(width/Math.max(x1.domain()[1]-x1.domain()[0],1)- 1));
     //debugger;
     //focus.select(".x.axis").call(xAxis);
 }
