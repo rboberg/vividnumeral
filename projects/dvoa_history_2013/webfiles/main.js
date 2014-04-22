050d5f0b6b21c95ebcaf969a9c7a00cd1dd60aa0
Ross Boberg <ross.boberg@moorecap.com>
2014-04-22 15:21:54 -0400

    create global js directory. add moving average slider to dvoa page

--- projects/dvoa_history_2013/webfiles/main.js
+++ projects/dvoa_history_2013/webfiles/main.js
@@ -1,12 +1,16 @@
+////////////////////////////////
 // BUTTON DIV
+////////////////////////////////
+
 var bheight = 20,
 bwidth = 50,
 bcol = 7,
 brow = 5,
-borderpx = 1;
+borderpx = 1
+ptop = 5;
 
 var bdiv = d3.select("#team_button_div");
-bdiv.style({'width':((bwidth + borderpx*2) * bcol) + 'px','height':((bheight + borderpx*2) * brow) + 'px'});
+bdiv.style({'width':((bwidth + borderpx*2) * bcol) + 'px','height':((bheight + borderpx*2 + ptop) * brow) + 'px'});
 
 d3.csv('webfiles/team_info.csv',function(data){
 
@@ -30,10 +34,10 @@ d3.csv('webfiles/team_info.csv',function(data){
 			d3.select(this).style({'border-color':'white'})
 		});
 
-		function colorThis(d, me, changeBool){
+		function colorThis(d, me, changeClass){
 			var highButton = me.classList.contains('highButton')
 
-			if((highButton & !changeBool) | (!highButton & changeBool)){
+			if((highButton & !changeClass) | (!highButton & changeClass)){
 				d3.select(me).style({'background-color':d.ColorSolo,'color':d.ColorLight})
 				.classed('highButton',true);
 			} else{
@@ -43,6 +47,42 @@ d3.csv('webfiles/team_info.csv',function(data){
 		}
 })
 
+//sets up tool tip for button labels
+$(function() {
+   	$( document ).tooltip();
+});
 
-// Interactive Time Series
+///////////////////////////////////
+// Interactive Time Series w/ MA
+///////////////////////////////////
+
+// Build MA Chooser
+var maxma = 20;
+
+$(function() {
+	var slider = $( "#ma_slider" ).slider({
+	  min: 1,
+	  max: maxma,
+	  range: "min",
+	  value: 1,
+	  orientation: 'vertical',
+	  slide: function( event, ui ) {
+	    $('#ma_label').text('MA=' + ui.value);
+	  }
+	});
+	$("#ma_label" ).text('MA=' + $('#ma_slider').slider('value'));
+});
+
+
+
+
+// Build Series Selector
+/*
+var select = $('<select />');
+for(var val in data) {
+    $('<option />', {value: val, text: data[val]}).appendTo(s);
+}
+*/
+
+
 
