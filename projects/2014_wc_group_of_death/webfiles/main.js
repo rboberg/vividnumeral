75f0573f1b615b87a5526c5b8386c9c2faaa39fa
Ross Boberg <ross.boberg@moorecap.com>
2014-06-06 13:04:07 -0400

    add label toggel to scatterplot. reformat axis labels. add deadliest and friendliest tables

--- projects/2014_wc_group_of_death/webfiles/main.js
+++ projects/2014_wc_group_of_death/webfiles/main.js
@@ -1,10 +1,18 @@
+/*
+TO DO
+* Re-format Axis
+* Button to Show all Names?
+* Center buttons across the top?
+*/
+
 //function to set highlighted group
 function setHL(grp){
+	global_higrp = grp === global_higrp ? 'none' : grp
+
 	d3.selectAll('.hilite').classed('hilite',false)
-	d3.selectAll('.'+grp).classed('hilite',true)
+	d3.selectAll('.'+global_higrp).classed('hilite',true)
 
 	d3.selectAll('circle').sort(function(a,b){return a.group===grp ? 1 : b.group===grp ? -1 : 0})
-
 	
 	d3.selectAll('.point_label').sort(function(a,b){return a.group===grp ? 1 : b.group===grp ? -1 : 0})
 	.style('fill',point_label_fill)
@@ -19,12 +27,13 @@ function setHL(grp){
 
 
 //Set up dimensions
-var margins = {top: 10, right: 10, bottom: 35, left: 60},
+var margins = {top: 30, right: 10, bottom: 40, left: 60},
 height = 400,
 width = 600 - margins.left - margins.right;
 
 //Global variable to control label visibility default
 global_visibility = 'hidden'
+global_higrp = 'none'
 
 //Set up axes
 var x = d3.scale.log().range([0, width]),
@@ -54,10 +63,24 @@ gdiv.selectAll('div')
 .enter()
 .append('div')
 	.attr('class',function(d){return 'group_button ' + d})
-	.style({'width':60 + 'px','height':30 + 'px','border-width':2 + 'px'})
+	.style({'width':46 + 'px','border-width':2 + 'px'})
 	.style('background-color',function(d){return midColors[groups.indexOf(d)]})
 	.text(function(d){return d})
 	.on('click',function(d){setHL(d)})
+
+//Toggle label button
+d3.selectAll('#label_button')
+	.on('click',function(){
+		global_visibility = global_visibility === 'visible' ? 'hidden' : 'visible'
+		//debugger;
+		d3.select(this).classed('show_label',global_visibility === 'visible')
+
+		d3.selectAll('.point_label')
+		.style('visibility',global_visibility)
+
+		d3.selectAll('.point_label.hilite')
+		.style('visibility','visible')
+	})
 
 //set up scatter plot
 var scatter = svg.append("g").attr('transform','translate('+margins.left+','+margins.top+')');
@@ -75,13 +98,15 @@ d3.json('webfiles/through_prob.json',function(data){
 	for(var i = 0; i<data.length;i++){tempAr.push(0)}
 	var xShift = _.object(_.pluck(data,"team"),tempAr)
 	// Manually shift some labels
-	yShift['Mexico'] = 22
+	xShift['Mexico'] = 5
+	yShift['Mexico'] = 20
+	xShift['Ghana'] = 15
 	yShift['Ghana'] = 22
 	yShift['Uruguay'] = 22
 	yShift['Italy'] = 22
-	yShift['England'] = 22
 	yShift['United States'] = 22
-	yShift['Japan'] = 20
+	xShift['Japan'] = 40
+	yShift['Japan'] = 5
 	yShift['Colombia'] = 20
 	yShift['Greece'] = 20
 	xShift['Bosnia and Herzegovina'] = -100
@@ -93,10 +118,12 @@ d3.json('webfiles/through_prob.json',function(data){
 	yShift['Honduras'] = 15
 	xShift['Ecuador'] = 70
 	yShift['Ecuador'] = 10
-	xShift['England'] = 90
+	xShift['England'] = 80
 	yShift['England'] = 20
-	xShift['Australia'] = 30
-	yShift['Australia'] = 22
+	xShift['Australia'] = 15
+	yShift['Australia'] = 23
+	xShift['Korea Republic'] = 65
+	yShift['Korea Republic'] = 20
 
 	xrg = d3.extent(_.pluck(data,axisVar[0].ugly));
 	yrg = d3.extent(_.pluck(data,axisVar[1].ugly));
@@ -151,7 +178,7 @@ d3.json('webfiles/through_prob.json',function(data){
 	.append("text")
 		.attr("class", "label")
 		.attr("x", width/2)
-		.attr("y", 30)
+		.attr("y", 35)
 		.style("text-anchor", "middle")
 		.text(axisVar[0].pretty);
 
@@ -164,13 +191,134 @@ d3.json('webfiles/through_prob.json',function(data){
 		.call(yAxis)
 	.append("text")
 		.attr("class", "label")
-		.attr("transform", "rotate(-90)translate(-150,0)")
-		.attr("y", -50)
+		.attr("transform", "rotate(-90)translate(0,0)")
+		.attr('x', -height/2)
+		.attr("y", -55)
 		.attr("dy", ".71em")
 		.style("text-anchor", "middle")
 		.text(axisVar[1].pretty);
 
 	//debugger;
 	})
+
+
+
+
+
+
+// Deadliest Table
+csvIn = 'webfiles/god_top.csv'
+COLUMNS = [
+{colname:'year',coltext:'Year',coltype:'tdtext',colgroup:'Deadliest Groups Since 2002'},
+{colname:'rd',coltext:'Group',coltype:'tdtext',colgroup:'Deadliest Groups Since 2002'},
+{colname:'pct',coltext:'% of Points',coltype:'tdvalue',colgroup:'Deadliest Groups Since 2002'},
+]
+tableid = '#god_top'
+d3csvTable(csvIn, COLUMNS, tableid);
+
+
+// Friendliest Table
+csvIn = 'webfiles/god_bottom.csv'
+COLUMNS = [
+{colname:'year',coltext:'Year',coltype:'tdtext',colgroup:'Friendliest Groups Since 2002'},
+{colname:'rd',coltext:'Group',coltype:'tdtext',colgroup:'Friendliest Groups Since 2002'},
+{colname:'pct',coltext:'% of Points',coltype:'tdvalue',colgroup:'Friendliest Groups Since 2002'},
+]
+tableid = '#god_bottom'
+d3csvTable(csvIn, COLUMNS, tableid);
+
+d3.select(d3.selectAll('#god_top tr:not(.thead)')[0][1]).style("color","skyblue")
+
+
+// FUNCTION TO MAKE PRETTY TABLES WITH BARS
+function d3csvTable(csvIn, COLUMNS, tableid){
+    d3.csv(csvIn, function(data) {
+
+        //set operative table from ID
+        var table = d3.select(tableid);
+
+        // CREATE HEADERS FROM INPUT INFORMATION
+        // MAY BE TWO ROWS OF HEADERS DEPENDING IF ON SOME ARE GROUPED
+        var HEAD1 = new Array();
+        var HEAD2 = _.filter(COLUMNS,function(o){return o.colgroup !== null})
+        var whichgrp;
+        //Group headers if there are any
+         _.each(COLUMNS,function(o){
+            if(o.colgroup === null){
+                HEAD1.push({coltext:o.coltext,coltype:o.coltype,ncol:1,nrow:HEAD2.length >0 ? 2 : 1})
+            } else{
+                whichgrp = _.indexOf(_.pluck(HEAD1,'coltext'),o.colgroup)
+                if(whichgrp===-1){
+                    HEAD1.push({colname:o.colgroup,coltext:o.colgroup,coltype:'thgroup',ncol:1,nrow:1})
+                }else{
+                    HEAD1[whichgrp].ncol ++
+                }
+            }
+            
+        })
+
+        
+        //Create the first header row
+        table.append('tr').attr('class','thead')
+        .selectAll('th')
+        .data(HEAD1)
+        .enter()
+        .append('th')
+        .text(function(column){return column.coltext})
+        .attr('class',function(column){return column.coltype})
+        .attr('colspan',function(column){return column.ncol})
+        .attr('rowspan',function(column){return column.nrow});
+
+        //If any columns were grouped create their individual headers
+        if(HEAD2.length > 0){
+            table.append('tr').attr('class','thead')
+            .selectAll('th')
+            .data(HEAD2)
+            .enter()
+            .append('th')
+            .text(function(column){return column.coltext})
+            .attr('class',function(column){return column.coltype});
+        }
+
+        //Create Rows
+        var rows = table.selectAll('tr:not(.thead)')
+        .data(data)
+        .enter()
+        .append('tr')
+        .classed('trtotal',function(d){return (d['Position']==='Total')})
+        .classed('trhilite',function(d){
+        	//debugger;
+        	return (d.rd =="G") & (d.year == '2014')
+        });
+
+        //Create Cells
+        var cells = rows.selectAll('td')
+        .data(function(row){
+            return _.map(COLUMNS,function(column){
+                return {column:column.colname,cvalue:row[column.colname],ctype:column.coltype};
+            });
+        })
+        .enter()
+        .append('td')
+        .attr('class',function(d){
+            //Set Cell class (tdvalue, tdtext, tdbar)
+            return d.ctype;
+        });
+
+        //format tdtext
+        rows.selectAll('.tdtext')
+        .text(function(d){return d.cvalue})
+
+        //format tdvalue
+        rows.selectAll('.tdvalue')
+        .text(function(d){
+        	var toprint = (Math.round(d.cvalue*10000)/100)
+        	return toprint.toFixed(2) + '%'
+        })
+    })   
+}
+
+
+
 
 
