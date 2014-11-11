7baa2c54c96886e0f04e208f2ae033287e01b060
Joe Morales <joseph.morales@flukenetworks.com>
2014-11-11 06:53:37 -0700

    Loads and shows all the charts.  Several TODOs

--- projects/fe_fall2014_chart_game/webfiles/main.js
+++ projects/fe_fall2014_chart_game/webfiles/main.js
@@ -1,65 +1,131 @@
+var charts = [];
+var chartIndices = [];
+var displayedChart = null;
+
+function initialize() {
+    d3.csv('webfiles/CHART_SERIES.csv', loadCharts);
+}
+
+function loadCharts(data) {
+    var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+    var currentChartId = -1;
+    var currentChart = {id: -1};
+
+    data.forEach(function (row) {
+        if (row.groupid !== currentChart.id) {
+            currentChart = {
+                id: row.groupid,
+                group: row.group,
+                data: []
+            };
+            charts.push(currentChart);
+        }
+        currentChart.data.push({
+            // TODO: did we want 'anonymous' dates?
+            date: parseDate(row.date),
+            close: parseFloat(row.value)
+        });
+    });
+
+    randomize();
+
+    // Ready
+    $('#nextButton').click(nextChart);
+    $('#nextButton').show();
+}
+
+function randomize() {
+    chartIndices = _.range(charts.length);
+
+    // TODO: block randomization
+    // chartIndices = _.shuffle(chartIndices);
+
+    chartIndices.reverse(); // so that we can use pop()
+}
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-width = 600 - margin.left - margin.right,
-height = 300 - margin.top - margin.bottom;
- 
-var parseDate = d3.time.format("%Y-%m-%d").parse;
- 
-var x = d3.time.scale()
-.range([0, width]);
- 
-var y = d3.scale.linear()
-.range([height, 0]);
- 
-var xAxis = d3.svg.axis()
-.scale(x)
-.orient("bottom");
- 
-var yAxis = d3.svg.axis()
-.scale(y)
-.orient("left");
- 
-var line = d3.svg.line()
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.close); });
- 
-//var svg = d3.select("body").append("svg")
-var svg = d3.select("#main_chart").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function nextChart() {
+    var ratingControls = $('#controls');
+    if (ratingControls[0].hidden) {
+        $('#nextButton').text('Next Chart');
+        ratingControls.show();
 
-csvIn = 'webfiles/aapl.csv'
+    } else {
+        // TODO: record results
+        // TODO: reset controls
+    }
 
-d3.csv(csvIn, function(data) {
+    $('#main_content').empty();
 
-	data.forEach(function(d) {
-	d.date = parseDate(d.date);
-	d.close = +d.close;
-	});
-	 
-	x.domain(d3.extent(data, function(d) { return d.date; }));
-	y.domain(d3.extent(data, function(d) { return d.close; }));
-	 
-	svg.append("g")
-	.attr("class", "x axis")
-	.attr("transform", "translate(0," + height + ")")
-	.call(xAxis);
-	 
-	svg.append("g")
-	.attr("class", "y axis")
-	.call(yAxis)
-	.append("text")
-	.attr("transform", "rotate(-90)")
-	.attr("y", 6)
-	.attr("dy", ".71em")
-	.style("text-anchor", "end")
-	.text("Price ($)");
-	 
-	svg.append("path")
-	.datum(data)
-	.attr("class", "line")
-	.attr("d", line);
-});
+    if (chartIndices.length > 0) {
+        displayedChart = charts[chartIndices.pop()];
+        $('#chartId').text(displayedChart.id);
+        drawChart(displayedChart.data);
+
+    } else {
+        done();
+    }
+
+}
+
+function done() {
+    $('#controls').show(false);
+    // TODO
+    $('#main_content').text('Done');
+}
+
+function drawChart(data) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 600 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function (d) { return x(d.date); })
+        .y(function (d) { return y(d.close); });
+
+    //var svg = d3.select("body").append("svg")
+    var svg = d3.select("#main_content").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(d3.extent(data, function (d) { return d.date; }));
+    y.domain(d3.extent(data, function (d) { return d.close; }));
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Price ($)");
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+}
